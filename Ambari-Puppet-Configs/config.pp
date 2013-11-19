3933a51761e9a48f125399a2777ab11efdcf9e68
Kristopher Kane <kkane@hortonworks.com>
2013-11-19 12:50:18 -0500

    Added notes

--- Ambari-Puppet-Configs/config.pp
+++ Ambari-Puppet-Configs/config.pp
@@ -1,2 +1,5 @@
 #Add the following line below the current Oozie check to the config.pp
+#Located here: /var/lib/ambari-agent/puppet/modules/hdp-nagios/manifests/server
+#Place this below the other oozie line
+#DO NOT overwrite the entire file with this one.
 hdp-nagios::server::check { 'check_oozie_workflows.py': }
