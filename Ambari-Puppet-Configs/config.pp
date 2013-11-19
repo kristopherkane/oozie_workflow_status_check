d0cc0dc2ab3410296e2b305736f4867a7613de23
Kristopher Kane <kkane@hortonworks.com>
2013-11-19 13:47:55 -0500

    Added full puppet path

--- Ambari-Puppet-Configs/config.pp
+++ Ambari-Puppet-Configs/config.pp
@@ -1,5 +1,5 @@
 #Add the following line below the current Oozie check to the config.pp
-#Located here: /var/lib/ambari-agent/puppet/modules/hdp-nagios/manifests/server
+#This definition should go on the Nagios host at: /var/lib/ambari-agent/puppet/modules/hdp-nagios/manifests/server/config.pp
 #Place this below the other oozie line
 #DO NOT overwrite the entire file with this one.
 hdp-nagios::server::check { 'check_oozie_workflows.py': }
